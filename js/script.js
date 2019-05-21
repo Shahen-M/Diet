ef4c362e0d986d036dd3f2e7aad0503302984b94
Shahen-M <smatevosyan2653@gmail.com>
2019-05-20 17:25:34 -0700

    added food pie chart and created links for each

--- js/script.js
+++ js/script.js
@@ -1,28 +1,31 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'pie',
-
-    // The data for our dataset
-    data: {
-        labels: ['Fruits', 'Grains', 'Protein', 'Vegetables'],
-        datasets: [{
-            fillColor: "#79D1CF",
-            strokeColor: "#79D1CF",
-            backgroundColor: [
-                'rgba(255, 0, 0, 0.1)',
-                'rgb(150, 128, 104)',
-                'rgba(255, 0, 0, 0.56)',
-                'rgba(0, 255, 0, 1)'
-            ],
-            data: [60, 80, 81, 56]
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
-});
-
+// var ctx = document.getElementById('myChart').getContext('2d');
+// var chart = new Chart(ctx, {
+//
+//     // The type of chart we want to create
+//     type: 'pie',
+//
+//     // The data for our dataset
+//     data: {
+//         labels: ['Fruits', 'Grains', 'Protein', 'Vegetables'],
+//         datasets: [{
+//             fillColor: "#79D1CF",
+//             strokeColor: "#79D1CF",
+//             backgroundColor: [
+//                 'rgba(255, 0, 0, 0.1)',
+//                 'rgb(150, 128, 104)',
+//                 'rgba(255, 0, 0, 0.56)',
+//                 'rgba(0, 255, 0, 1)'
+//             ],
+//             data: [60, 80, 81, 56]
+//         }]
+//     },
+//
+//     // Configuration options go here
+//     options: {
+//       responsive: false
+//     }
+// });
+//
 
 
 
