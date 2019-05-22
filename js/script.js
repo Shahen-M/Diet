33db86fb4efc4057f8aca54be797abb7d4ace22e
Shahen-M <smatevosyan2653@gmail.com>
2019-05-21 21:14:39 -0700

    added BMI calculator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 
+document.getElementById('calculate_btn').addEventListener('click', function() {
+  $('.calculator_popup').show();
+
+  bmiCalculate();
+})
+
+function bmiCalculate() {
+  const height_ft = document.getElementById('height_in_ft').value;
+  const height_in = document.getElementById('height_in_inches').value;
+  const weight = document.getElementById('weight').value;
+  let bmi_num = document.getElementById('bmi_number');
+
+  let weight_cal = weight * 703
+  let height_cal = height_ft * 12 + parseInt(height_in);
+  let height_final = height_cal * height_cal;
+
+  let result = weight_cal / height_final;
+
+  bmi_num.innerHTML = 'Your BMI is ' + result;
+}
