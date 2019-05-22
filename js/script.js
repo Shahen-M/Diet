45ad51b4703ddda7259450fae503c9beb759a2ae
Shahen-M <smatevosyan2653@gmail.com>
2019-05-21 23:17:43 -0700

    added some functonailty to BMI calculator

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 
 document.getElementById('calculate_btn').addEventListener('click', function() {
-  $('.calculator_popup').show();
-
+  checkIfTextBoxEmpty();
   bmiCalculate();
 })
 
@@ -14,8 +13,67 @@ function bmiCalculate() {
   let weight_cal = weight * 703
   let height_cal = height_ft * 12 + parseInt(height_in);
   let height_final = height_cal * height_cal;
-
   let result = weight_cal / height_final;
 
   bmi_num.innerHTML = 'Your BMI is ' + result;
 }
+
+function checkIfTextBoxEmpty() {
+  const height_ft = document.getElementById('height_in_ft').value;
+  const height_in = document.getElementById('height_in_inches').value;
+  const weight = document.getElementById('weight').value;
+  let maleRadio = document.getElementById('male_radio').checked = false;
+  let femaleRadio = document.getElementById('female_radio').checked = false;
+
+  if(height_ft.length < 1 && weight.length < 1) {
+    alert('Please fill out missing fields');
+  }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* */
