ce56459791fec4541178dbbb528a61d565e01c20
Shahen-M <smatevosyan2653@gmail.com>
2019-05-22 23:14:44 -0700

    adde functionaily to TDEE calculator

--- js/script.js
+++ js/script.js
@@ -21,6 +21,10 @@ document.getElementById('bmi_popup').addEventListener('click', function() {
   $('.calculator_content').show();
 })
 
+document.getElementById('daily_calorie_btn').addEventListener('click', function() {
+  calculateTdee();
+})
+
 function bmiCalculate() {
   const height_ft = document.getElementById('height_in_ft').value;
   const height_in = document.getElementById('height_in_inches').value;
@@ -59,20 +63,47 @@ function clearFields() {
   const weight = document.getElementById('weight').value = '';
   let maleRadio = document.getElementById('male_radio').checked = false;
   let femaleRadio = document.getElementById('female_radio').checked = false;
+}
+
+function calculateTdee() {
+  const height = document.getElementById('daily_calorie_height').value;
+  const weight = document.getElementById('daily_calorie_weight').value;
+  const age = document.getElementById('daily_calorie_age').value;
+  const activity = document.getElementById('activity_level').value;
+
+  let maleRadio = document.getElementById('daily_male_radio');
+  let femaleRadio = document.getElementById('daily_female_radio');
+
+
+  let female_bmr = (height * 6.25) + (weight * 9.99) - (age * 4.92) - 161;
+  let male_bmr = (height * 6.25) + (weight * 9.99) - (age * 4.92) + 5;
+
+  if(femaleRadio.checked == true) {
+    if(activity == 1) {
+        var sedentary = female_bmr * 1.1;
+      } else if (activity == 2) {
+        var light = female_bmr * 1.275;
+      } else if (activity == 3) {
+        var moderate = female_bmr * 1.35;
+      } else if (activity == 4) {
+        var heavy = female_bmr * 1.525;
+      }
+  }
+
+  if(maleRadio.checked == true) {
+    if(activity == 1) {
+        var sedentary = male_bmr * 1.2;
+      } else if (activity == 2) {
+        var light = male_bmr * 1.375;
+      } else if (activity == 3) {
+        var moderate = male_bmr * 1.55;
+      } else if (activity == 4) {
+        var heavy = male_bmr * 1.725;
+      }
+  }
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
